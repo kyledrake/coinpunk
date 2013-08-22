c60cf4ad1866bb42d258e94c7e41b0b61ec09a95
Kyle Drake <kyledrake@gmail.com>
2013-08-21 17:40:11 -0700

    tests for wallet unspent tx

--- public/test/wallet.js
+++ public/test/wallet.js
@@ -1,13 +1,29 @@
 
 describe('coinpunk.Wallet', function(){
   
+  var unspentTxs = [{
+    "txid" : "54d5437ee8a55f25c88c836fbab7556f9088dbbdc4ffa4afbb7f1c4291a330ad",
+    "vout" : 0,
+    "scriptPubKey" : "76a914168e48aa5551a3ce7339dd55048b976edea3687288ac",
+    "amount" : 0.06000000,
+    "amountSatoshi" : 6000000,
+    "confirmations" : 1164
+  }, {
+    "txid" : "54d5437ee8a55f25c88c836fbab7556f9088dbbdc4ffa4afbb7f1c4291a330ad",
+    "vout" : 0,
+    "scriptPubKey" : "76a914168e48aa5551a3ce7339dd55048b976edea3687288ac",
+    "amount" : 0.06000000,
+    "amountSatoshi" : 6000000,
+    "confirmations" : 1164
+  }]
+  
   it('should create new addresses', function() {
     var wallet = new coinpunk.Wallet();
     var address = wallet.createNewAddress('Default');
     assert.equal(address, wallet.addresses()[0].address);
     assert.equal('Default', wallet.addresses()[0].name);
   })
-  
+
   it('should create a wallet key from id and pass', function() {
     var id = 'test@example.com',
         pass = 'p4$$w0rdl0ll0l';
@@ -31,4 +47,17 @@ describe('coinpunk.Wallet', function(){
     assert.equal(w.addresses()[0].address, address);
     assert.equal(w.addresses()[0].name, 'Test Name');
   })
+
+  it('should load unspent txs', function() {
+    w = new coinpunk.Wallet();
+    w.setUnspentTxs(unspentTxs);
+    assert.equal(w.unspentBalance(), 0.12);
+  });
+  
+  it('should create a transaction', function() {
+    w = new coinpunk.Wallet();
+    w.setUnspentTxs(unspentTxs);
+    var rawTx = w.createSend('0.06', '0.0005', '134GKGyWFftj2m4ZFKsBuCbm3GgXfDmuxX');
+    
+  });
 })
