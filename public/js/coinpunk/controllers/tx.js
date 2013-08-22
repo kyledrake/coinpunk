f340ca84659e2287b37214afcb498867323cd809
Kyle Drake <kyledrake@gmail.com>
2013-08-21 17:36:19 -0700

    remove unused self=this

--- public/js/coinpunk/controllers/tx.js
+++ public/js/coinpunk/controllers/tx.js
@@ -2,8 +2,6 @@ coinpunk.controllers.Tx = function() {};
 coinpunk.controllers.Tx.prototype = new coinpunk.Controller();
 
 coinpunk.controllers.Tx.prototype.details = function(txid) {
-  var self = this;
-
   $.get('/api/tx/details', {txid: txid}, function(resp) {
     coinpunk.router.render('view', 'tx/details', {tx: resp.tx});
   });
