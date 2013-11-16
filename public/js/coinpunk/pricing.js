3f5dc397ca6b5af29413fc53c2258a4e1a2b3ec6
Kyle Drake <kyledrake@gmail.com>
2013-11-16 19:07:40 +0800

    queue up weighted prices requests

--- public/js/coinpunk/pricing.js
+++ public/js/coinpunk/pricing.js
@@ -3,20 +3,33 @@ coinpunk.pricing = {
   pricesApiUrl: '/api/weighted_prices',
   defaultCurrency: 'USD',
 
+  queuedRequests: [],
+
   getLatest: function(callback) {
     var self = this;
-    var currency = this.getCurrency();
+
+    if(this.inProgress == true)
+      return this.queuedRequests.push(callback);
 
     if(!self.cachedResponse ||
        !self.cachedResponseTime ||
        ((new Date().getTime()/1000) - self.cachedResponseTime) > self.cacheTimeout) {
 
+      this.inProgress = true;
+
       $.get(this.pricesApiUrl, function(response) {
         if(response.error)
           return;
+
         self.cachedResponse = response;
         self.cachedResponseTime = (new Date().getTime()/1000);
+
         self.runCallback(callback);
+
+        while(self.queuedRequests.length != 0)
+          self.runCallback(self.queuedRequests.pop());
+
+        self.inProgress = false;
       });
     } else {
       this.runCallback(callback);
