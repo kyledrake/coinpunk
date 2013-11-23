c53a22f45cee0fd888ff268978b806aa7b98c44d
Kyle Drake <kyledrake@gmail.com>
2013-11-23 08:13:20 -0800

    precache view templates on initial load

--- public/js/coinpunk/template.js
+++ public/js/coinpunk/template.js
@@ -1,22 +1,53 @@
 coinpunk.Template = {
+  preCache: [
+    'dashboard/addresses',
+    'dashboard/received',
+    'dashboard/sent',
+    'tx/details',
+    'tx/send',
+    'backup',
+    'dashboard',
+    'header',
+    'node_error',
+    'signin',
+    'signup',
+    'settings'
+  ],
+
   templateCache: {},
+
+  get: function(path, callback) {
+    var self = this;
+
+    $.get('views/'+path+'.html', function(res) {
+      self.templateCache[path] = res;
+      if(callback)
+        callback(res);
+    });
+  },
 
   draw: function(id, path, data, callback) {
     var self = this;
 
     if(this.templateCache[path])
-      this._parseTemplate(id, this.templateCache[path], data, callback);
-    else  
-      $.get('views/'+path+'.html', function(res) {
-        self.templateCache[path] = res;
-        self._parseTemplate(id, res, data, callback);
+      this.parseTemplate(id, this.templateCache[path], data, callback);
+    else
+      this.get(path, function(res) {
+        self.parseTemplate(id, res, data, callback);
       });
   },
-  
-  _parseTemplate: function(id, template, data, callback) {
+
+  parseTemplate: function(id, template, data, callback) {
     $('#'+id).html(_.template(template, data, {variable: 'data'}));
 
     if(callback)
       callback(id);
+  },
+
+  loadPreCache: function() {
+    for(var i=0; i<this.preCache.length;i++)
+      this.get(this.preCache[i]);
   }
 };
+
+coinpunk.Template.loadPreCache();
