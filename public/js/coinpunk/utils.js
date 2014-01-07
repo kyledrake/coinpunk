40df4180e2c39b1470a6e73cd5800ea31410715c
Kyle Drake <kyle@kyledrake.net>
2014-01-06 18:34:37 -0800

    Escape values in templates properly, add utils

---
+++ public/js/coinpunk/utils.js
@@ -0,0 +1,11 @@
+coinpunk.utils = {};
+
+// https://github.com/component/escape-html
+coinpunk.utils.stripTag = function(html) {
+  return String(html)
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+};
