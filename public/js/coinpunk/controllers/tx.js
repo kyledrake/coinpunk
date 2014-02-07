f97d480f0169e7d6a524ba77d80baa9b2e34eff2
Kyle Drake <kyle@kyledrake.net>
2014-02-07 14:50:27 +0000

    support for QR codes from legacy bitcoin wallets

--- public/js/coinpunk/controllers/tx.js
+++ public/js/coinpunk/controllers/tx.js
@@ -188,7 +188,15 @@ coinpunk.controllers.Tx.prototype.scanQR = function(event) {
     if(result === 'error decoding QR Code')
       return errorsDiv.removeClass('hidden').text('Could not process the QR code, the image may be blurry. Please try again.');
 
+      console.log(result)
+
     var uri = new URI(result);
+
+    if(uri.protocol() == '') {
+      $('#address').val(uri.toString());
+      coinpunk.controllers.tx.calculateFee()
+      return;
+    }
 
     if(uri.protocol() != 'bitcoin')
       return errorsDiv.removeClass('hidden').text('Not a valid Bitcoin QR code.');
@@ -201,8 +209,11 @@ coinpunk.controllers.Tx.prototype.scanQR = function(event) {
     
     var queryHash = uri.search(true);
     
-    if(queryHash.amount)
+    if(queryHash.amount) {
       $('#amount').val(queryHash.amount);
+      coinpunk.controllers.tx.sendExchangeUpdate();
+      coinpunk.controllers.tx.calculateFee();
+    }
   }
 
   var canvas = document.createElement('canvas');
